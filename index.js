8ef50009a8e8d0b84e2f9a50659bb45c97a21080
hhhhhanyi <hhhhhanyi@gmail.com>
2019-06-24 22:16:03 +0800

    add page index

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const app = express();
+const bodyparser = require("body-parser");
 const bigQuery = require('./util/bigquery');
 
-app.get('/data', (req, res) => {
-	bigQuery('2014','09','24').then((result) => {
-	  const [rows] = result;
-	  res.send(rows);
-	});
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.post('/data', (req, res) => {
+	let date = req.body.date;
+	/* 檢查輸入格式 */
+	if (date && date.split('-').length === 3) {
+		/* 檢查輸入日期範圍 */
+		if (date > 1356969600000 && date < 1501516800000) {
+			date = date.split('-');
+			const year = date[0];
+			const month = date[1];
+			const day = date[2];
+			bigQuery(year, month, day).then((result) => {
+			  const [data] = result;
+				/* 檢查是否有資料內容 */
+				if (data.length > 0) {
+					res.send(JSON.stringify(data));
+				} else {
+					res.send(JSON.stringify({
+						error: 'No Data.'
+					}));
+				}
+			});
+		} else {
+			res.send(JSON.stringify({
+				error: 'Wrong Date.'
+			}));
+		}
+	} else {
+		res.send(JSON.stringify({
+			error: 'Wrong Format.'
+		}));
+	}
 });
 
+app.use('/', express.static('public'));
 app.listen(3000);
--- util/bigquery.js
+++ util/bigquery.js
@@ -1,5 +1,5 @@
 const {BigQuery} = require('@google-cloud/bigquery');
-const config = require('./config');
+const constants = require('./constants');
 
 const bigQuery = (year, month, day) => {
   const bigquery = new BigQuery({
@@ -8,17 +8,17 @@ const bigQuery = (year, month, day) => {
   });
 
   const query = `SELECT
-                   TIME_TRUNC(TIME(trip_start_timestamp), HOUR) AS HOUR,
-                   AVG(trip_total) AS TOTAL,
-                   AVG(fare) AS FARE,
-                   COUNT(*) AS COUNT
-                  FROM \`${config.projectId}.${config.dataset}.${config.table}\`
-                  WHERE DATE(trip_start_timestamp) = "${year}-${month}-${day}"
-                  GROUP BY HOUR ORDER BY HOUR`;
+    TIME_TRUNC(TIME(trip_start_timestamp), HOUR) AS HOUR,
+    AVG(trip_total) AS TOTAL,
+    AVG(tips) AS TIPS,
+    COUNT(*) AS COUNT
+   FROM \`${constants.projectId}.${constants.dataset}.${constants.table}\`
+   WHERE DATE(trip_start_timestamp) = "${year}-${month}-${day}"
+   GROUP BY HOUR ORDER BY HOUR`;
 
   const options = {
     query: query,
-    location: config.location
+    location: constants.location
   };
 
   return bigquery.query(options);
