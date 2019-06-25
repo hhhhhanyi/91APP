c2c3d634d17429b729af45613538abe556a63b97
hhhhhanyi <hhhhhanyi@gmail.com>
2019-06-25 16:45:03 +0800

    modify loading

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ const search = () => {
   document.getElementsByClassName("subtitle")[1].innerHTML = '';
   document.getElementById("statistics").innerHTML = '';
   document.getElementById("chart").innerHTML = '';
-  document.getElementById("errorMessage").innerHTML = 'loading.....';
+  document.getElementById("message").innerHTML = 'loading.....';
   fetch('/data', {
     method: 'POST',
     headers: {
@@ -17,7 +17,7 @@ const search = () => {
     return response.json();
   }).then((data) => {
     if (!data.error) {
-      document.getElementById("errorMessage").innerHTML = '';
+      document.getElementById("message").innerHTML = '';
       createTable(data);
       createChart(data);
       document.getElementsByClassName("title")[0].innerHTML = 'TABLE';
@@ -25,7 +25,7 @@ const search = () => {
       document.getElementsByClassName("subtitle")[0].innerHTML = `${date} 中 24 個小時的平均車費、平均小費、小費／車費百分比、車次`;
       document.getElementsByClassName("subtitle")[1].innerHTML = `${date} 中 24 個小時的平均車費`;
     } else {
-      document.getElementById("errorMessage").innerHTML = data.error;
+      document.getElementById("message").innerHTML = data.error;
     }
   }).catch((error) => {
     console.log(error);
