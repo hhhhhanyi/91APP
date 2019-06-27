5f11fd356fd87761ee6db5862eea4dc66f14661c
hhhhhanyi <hhhhhanyi@gmail.com>
2019-06-27 09:37:47 +0800

    fixbugs

--- public/js/index.js
+++ public/js/index.js
@@ -23,8 +23,8 @@ const search = () => {
       createChart(data);
       document.getElementsByClassName("title")[0].innerHTML = 'TABLE';
       document.getElementsByClassName("title")[1].innerHTML = 'CHART';
-      document.getElementsByClassName("subtitle")[0].innerHTML = `${date} 中 24 個小時的平均車費、平均小費、小費／車費百分比、車次`;
-      document.getElementsByClassName("subtitle")[1].innerHTML = `${date} 中 24 個小時的平均車費`;
+      document.getElementsByClassName("subtitle")[0].innerHTML = `${year}-${month} 中 24 個小時的平均車費、平均小費、小費／車費百分比、車次`;
+      document.getElementsByClassName("subtitle")[1].innerHTML = `${year}-${month} 中 24 個小時的平均車費`;
     } else {
       document.getElementById("message").innerHTML = data.error;
     }
